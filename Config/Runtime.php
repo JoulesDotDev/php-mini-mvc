b644186180bfd8364d897c91cb8c28fad1ccfcac
JoulesDotDev <73104228+JoulesDotDev@users.noreply.github.com>
2023-06-30 12:55:51 +0200

    Update Runtime.php

--- Config/Runtime.php
+++ Config/Runtime.php
@@ -4,3 +4,5 @@ define('GET', $_SERVER['REQUEST_METHOD'] === "GET");
 define('POST', $_SERVER['REQUEST_METHOD'] === "POST");
 
 define('ACTION', isset($_POST['action']) ? $_POST['action'] : null);
+
+//TODO Allowed-Methods Header
