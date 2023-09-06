5c3a7ddf590973391b2000cb26422ca196fd1a05
JoulesDotDev <_@joules.dev>
2023-09-06 19:48:04 +0200

    global try catch

--- index.php
+++ index.php
@@ -4,5 +4,15 @@ define('ROOT_DIR', dirname(__FILE__));
 require_once "Config/Initialize.php";
 require_once ROOT_DIR . "/Controllers/Utils/Routing/Router.php";
 
-$router = new Router();
-$router->route();
+if (PRODUCTION) {
+    try {
+        $router = new Router();
+        $router->route();
+    } catch (Exception | Error $e) {
+        Log::Error($e);
+        JSON("Something went terribly wrong.", 500);
+    }
+} else {
+    $router = new Router();
+    $router->route();
+}
