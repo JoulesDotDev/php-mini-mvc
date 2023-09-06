227b4b30fe883eda45182a28a5a678b778f9b26d
JoulesDotDev <_@joules.dev>
2023-09-06 18:34:09 +0200

    ext

--- Views/Components/Layout/BaseHeader.php
+++ Views/Components/Layout/BaseHeader.php
@@ -6,6 +6,8 @@
     <script src="https://unpkg.com/htmx.org@1.9.4" crossorigin="anonymous"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/morph@3.x.x/dist/cdn.min.js"></script>
+
     <script src="//<?= BASE_URL ?>/static/scripts/main.js"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/daisyui@3.6.2/dist/full.css" rel="stylesheet" type="text/css" />
