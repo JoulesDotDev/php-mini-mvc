b10f16cafb0859b0c0efef76ecbd4a56cd9e1ca1
JoulesDotDev <_@joules.dev>
2023-09-09 14:33:40 +0200

    dynamically load css and scripts per page

--- Views/Components/Layout/BaseHeader.php
+++ Views/Components/Layout/BaseHeader.php
@@ -6,12 +6,19 @@
     <script src="https://unpkg.com/htmx.org@1.9.4" crossorigin="anonymous"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
     <script src="//<?= BASE_URL ?>/static/scripts/main.js"></script>
+
+    <?php foreach ((_CONTEXT("head", "scripts") ?? []) as $script) : ?>
+        <script src="//<?= BASE_URL ?>/static/scripts/<?= $script ?>.js"></script>
+    <?php endforeach; ?>
 
     <link href="https://cdn.jsdelivr.net/npm/daisyui@3.6.2/dist/full.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="//<?= BASE_URL ?>/static/css/main.css" rel="stylesheet" type="text/css" />
+
+    <?php foreach ((_CONTEXT("head", "styles") ?? []) as $style) : ?>
+        <script src="//<?= BASE_URL ?>/static/css/<?= $style ?>.css"></script>
+    <?php endforeach; ?>
 </head>
 
 <body id="#body" hx-boost="true" hx-indicator=".htmx-progress">
